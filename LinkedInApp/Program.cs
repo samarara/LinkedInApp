111b5fd6d62db77451f35bcdada404455ad519f6
samarara <samarara@SAMARARA-MAC>
2016-10-15 15:04:51 -0400

    Add project files.

---
+++ LinkedInApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedInApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
