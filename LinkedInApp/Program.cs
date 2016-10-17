444560681b6e2f2ad1d83c5f651bdc26267310cf
samarara <samarara@SAMARARA-MAC>
2016-10-17 18:03:40 -0400

    initial commit

--- LinkedInApp/Program.cs
+++ LinkedInApp/Program.cs
@@ -10,6 +10,15 @@ namespace LinkedInApp
     {
         static void Main(string[] args)
         {
+            string userName = null;
+            string password = null;
+            Console.WriteLine("Enter the LinkedIn username:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            password = Console.ReadLine();
+
+            Console.ReadLine();
         }
     }
 }
+
